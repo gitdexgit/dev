553f8102006d78f76c6b59bdaf5ad56ed4259b14
gitdexgit <ttdextester@gmail.com>
2026-09-02 02:29:33 +0100

    Automated sync on Wed Sep  2 02:29:33 AM +01 2026

--- .config/i3/main_toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if i3-msg -t get_tree | grep -q '"class":"st_main"'; then
-    i3-msg '[class="st_main"] scratchpad show'
-else
-    mkdir -p ~/work
-    st -c "st_main" -t "main" -e tmux new-session -A -s main &
-    sleep 0.3
-    i3-msg '[class="st_main"] resize grow right 1 px'
-fi
--- scripts/f1_scrot.sh
+++ scripts/f1_scrot.sh
@@ -2,6 +2,12 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/dex/.Xauthority
 
-WID=$(xdotool getactivewindow)
+
+# No need
+# WID=$(xdotool getactivewindow)
+
 scrot -s /tmp/s.png && xclip -selection clipboard -t image/png -i /tmp/s.png && rm /tmp/s.png
-xdotool windowfocus "$WID"
+
+
+# No need ok
+# xdotool windowfocus "$WID"
